2056262a1ca413371bd68e0026f6ddc733a53900
Suraj Yadav <suraj.yadav@hostbooks.com>
2023-11-30 18:52:15 +0530

    Roll Back

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jobRouter = require('./routes/job_routes');
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log('connect with mongoose jobconnect db')
+    console.log('connect with mongoose jobconnect db');
 }).catch(err => console.log('error connecting',err));
 
 app.use(bodyParser.json());
@@ -17,4 +17,4 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use('/api/jobs',jobRouter);
 
-app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT || port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${process.env.PORT}!`))
